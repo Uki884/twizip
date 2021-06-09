45138110c6a448a86a78c8e6764185a8344797bc
Uki884 <mrchildreny@gmail.com>
2021-06-10 02:25:19 +0900

    fix url

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -2,8 +2,9 @@ import axios, { AxiosInstance } from 'axios'
 
 export const createAxiosClient = (): AxiosInstance => {
   const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   return axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: baseUrl || 'http://localhost:3000',
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
